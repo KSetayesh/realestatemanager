0655e3c428b27c840795c228305782fbd3fb8874
Kevin Setayesh <kevinsetayesh@gmail.com>
2024-05-09 16:20:43 -0400

    Working on listing.details.dto.builder.ts

--- packages/server/src/realestatecalc/builders/listing.details.dto.builder.ts
+++ packages/server/src/realestatecalc/builders/listing.details.dto.builder.ts
@@ -1,5 +1,6 @@
-import { Country, PropertyStatus, PropertyType, State } from "@realestatemanager/shared";
+import { Country, ListingDetailsDTO, PropertyStatus, PropertyType, State, Utility } from "@realestatemanager/shared";
 import { RentCastResponse } from "../models/rent_cast_api_models/rentcastresponse.model";
+import { convertSquareFeetToAcres } from "src/shared/Constants";
 
 type RentCastSaleResponseType = {
     id: string;
@@ -82,8 +83,103 @@ export class ListingDetailsDTOBuilder {
         this.rentCastPropertyResponses = rentCastPropertyResponses;
     }
 
-    build() {
-        
+    build(): ListingDetailsDTO[] {
+        const listingDetailsDTOList: ListingDetailsDTO[] = [];
+        const rentCastPropertyResponseMap: Map<string, RentCastPropertyResponseType> = this.createRentCastPropertyResponseTypeMap();
+        for (const rentCastSalesResponse of this.rentCastSalesResponses) {
+            const rentCastSalesResponseTyped: RentCastSaleResponseType = this.createRentCastSaleResponseType(rentCastSalesResponse);
+            const id = rentCastSalesResponseTyped.id;
+
+            /* 
+                Purposely not checking for id in map, 
+                if it doesnt exist we will simply pass in undefined into the buildListingDetailsDTO function
+            */
+            const rentCastPropertyTyped: RentCastPropertyResponseType = rentCastPropertyResponseMap.get(id);
+            const listingDetailsDTO: ListingDetailsDTO =
+                this.buildListingDetailsDTO(rentCastSalesResponseTyped, rentCastPropertyTyped);
+            listingDetailsDTOList.push(listingDetailsDTO);
+
+        }
+        return listingDetailsDTOList;
+    }
+
+    private createRentCastPropertyResponseTypeMap(): Map<string, RentCastPropertyResponseType> {
+        const rentCastPropertyResponseMap = new Map<string, RentCastPropertyResponseType>();
+        for (const rentCastPropertyResponse of this.rentCastPropertyResponses) {
+            const rentCastPropertyResponseTyped: RentCastPropertyResponseType =
+                this.createRentCastPropertyResponseType(rentCastPropertyResponse);
+            const id = rentCastPropertyResponseTyped.id;
+            if (!(id in rentCastPropertyResponseMap)) {
+                rentCastPropertyResponseMap.set(id, rentCastPropertyResponseTyped);
+            }
+        }
+        return rentCastPropertyResponseMap;
+    }
+
+    // TO-DO
+    private buildListingDetailsDTO(
+        rentCastSalesResponseTyped: RentCastSaleResponseType,
+        rentCastPropertyTyped?: RentCastPropertyResponseType,
+    ): ListingDetailsDTO {
+
+        const daysOnMarket = rentCastSalesResponseTyped.daysOnMarket ?? 0;
+        const listedDate = rentCastSalesResponseTyped.listedDate ?? Utility.getDateNDaysAgo(daysOnMarket);
+        const numberOfBathrooms = rentCastSalesResponseTyped.bathrooms ?? rentCastPropertyTyped?.bathrooms ?? -1;
+        const numberOfFullBathrooms = Math.floor(numberOfBathrooms);
+        const numberOfHalfBathrooms = Utility.isDecimal(numberOfBathrooms) ? 1 : 0;
+        const lotSize = rentCastSalesResponseTyped.lotSize ?? rentCastPropertyTyped?.lotSize;
+        const acres = lotSize ? convertSquareFeetToAcres(lotSize) : -1;
+
+        const listingDetail: ListingDetailsDTO = {
+            zillowURL: `NEED TO UPDATE_${rentCastSalesResponseTyped.id}`,
+            propertyDetails: {
+                address: {
+                    fullAddress: rentCastSalesResponseTyped.formattedAddress ?? rentCastPropertyTyped?.formattedAddress ?? '',
+                    state: rentCastSalesResponseTyped.state ?? rentCastPropertyTyped?.state, // Let it be undefined
+                    zipcode: rentCastSalesResponseTyped.zipCode ?? rentCastPropertyTyped?.zipCode ?? '',
+                    city: rentCastSalesResponseTyped.city ?? rentCastPropertyTyped?.city ?? '',
+                    county: rentCastSalesResponseTyped.county ?? rentCastPropertyTyped?.county ?? '',
+                    country: Country.UnitedStates,
+                    streetAddress: rentCastSalesResponseTyped.addressLine1 ?? rentCastPropertyTyped?.addressLine1 ?? '',
+                    apartmentNumber: rentCastSalesResponseTyped.addressLine2 ?? rentCastPropertyTyped?.addressLine2 ?? '',
+                    longitude: rentCastSalesResponseTyped.longitude ?? rentCastPropertyTyped?.longitude ?? -1,
+                    latitude: rentCastSalesResponseTyped.latitude ?? rentCastPropertyTyped?.latitude ?? -1,
+                },
+                schoolRating: {
+                    elementarySchoolRating: -1,
+                    middleSchoolRating: -1,
+                    highSchoolRating: -1,
+                },
+                numberOfBedrooms: rentCastSalesResponseTyped.bedrooms ?? rentCastPropertyTyped?.bedrooms ?? -1,
+                numberOfFullBathrooms: numberOfFullBathrooms,
+                numberOfHalfBathrooms: numberOfHalfBathrooms,
+                squareFeet: rentCastSalesResponseTyped.squareFootage ?? rentCastPropertyTyped?.squareFootage ?? -1,
+                acres: acres,
+                yearBuilt: rentCastSalesResponseTyped.yearBuilt ?? rentCastPropertyTyped?.yearBuilt ?? -1,
+                hasGarage: rentCastPropertyTyped?.features?.garage ?? false,
+                hasPool: rentCastPropertyTyped?.features?.pool ?? false,
+                hasBasement: false, // No info here
+                propertyType: rentCastSalesResponseTyped.propertyType ?? rentCastPropertyTyped?.propertyType, // Let it be undefined
+                description: '',
+            },
+            zillowMarketEstimates: {
+                zestimate: -1,
+                zestimateRange: {
+                    low: -1,
+                    high: -1,
+                },
+                zillowRentEstimate: -1,
+                zillowMonthlyPropertyTaxAmount: Utility.round((rentCastPropertyTyped?.previousYearPropertyTaxes / 12) ?? -1),
+                zillowMonthlyHomeInsuranceAmount: -1,
+                zillowMonthlyHOAFeesAmount: -1,
+            },
+            listingPrice: rentCastSalesResponseTyped.price ?? -1,
+            dateListed: listedDate,
+            numberOfDaysOnMarket: daysOnMarket,
+            propertyStatus: rentCastSalesResponseTyped.status, // Let it be undefined
+        };
+
+        return listingDetail;
     }
 
     private createRentCastSaleResponseType(rentCastSalesResponse: RentCastResponse): RentCastSaleResponseType {
@@ -112,6 +208,52 @@ export class ListingDetailsDTOBuilder {
             createdDate: jsonData.createdDate ?? new Date(0),
             lastSeenDate: jsonData.lastSeenDate ?? new Date(0),
             daysOnMarket: jsonData.daysOnMarket ?? 0,   // Come back to this and think about what to do here
+        };
+    }
+
+    private createRentCastPropertyResponseType(rentCastSalesResponse: RentCastResponse): RentCastPropertyResponseType {
+        const jsonData = rentCastSalesResponse.apiResponseData;
+        return {
+            id: rentCastSalesResponse.id,
+            formattedAddress: jsonData.formattedAddress ?? '',
+            addressLine1: jsonData.addressLine1 ?? '',
+            addressLine2: jsonData.addressLine2 ?? '',
+            city: jsonData.city ?? '',
+            state: jsonData.state ?? '',
+            zipCode: jsonData.zipCode ?? '',
+            county: jsonData.county ?? '',
+            bedrooms: jsonData.bedrooms ?? -1,
+            bathrooms: jsonData.bathrooms ?? -1,
+            latitude: jsonData.latitude ?? -1,
+            longitude: jsonData.longitude ?? -1,
+            squareFootage: jsonData.squareFootage ?? -1,
+            propertyType: jsonData.propertyType ?? '',
+            lotSize: jsonData.lotSize ?? -1,
+            yearBuilt: jsonData.yearBuilt ?? -1,
+
+            assessorID: jsonData.assessorID ?? '',
+            lastSalePrice: jsonData.lastSalePrice ?? -1,
+            lastSaleDate: jsonData.lastSaleDate ?? new Date(0),
+            ownerOccupied: jsonData.ownerOccupied ?? false,
+            features: {
+                garage: jsonData.features?.garage ?? false,
+                pool: jsonData.features?.pool ?? false,
+                floorCount: jsonData.features?.floorCount ?? -1,
+                unitCount: jsonData.features?.unitCount ?? -1
+            },
+            previousYearPropertyTaxes: jsonData.propertyTaxes[(new Date().getFullYear() - 1)]?.total ?? -1,
+            owner: {
+                names: jsonData.owner?.names ?? [],
+                mailingAddress: {
+                    id: jsonData.owner?.mailingAddress?.id ?? '',
+                    formattedAddress: jsonData.owner?.mailingAddress?.formattedAddress ?? '',
+                    addressLine1: jsonData.owner?.mailingAddress?.addressLine1 ?? '',
+                    addressLine2: jsonData.owner?.mailingAddress?.addressLine2 ?? '',
+                    city: jsonData.owner?.mailingAddress?.city ?? '',
+                    state: jsonData.owner?.mailingAddress?.state ?? '',
+                    zipCode: jsonData.owner?.mailingAddress?.zipCode ?? '',
+                },
+            },
         };
     }
 }
