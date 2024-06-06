fc09452d8f852b9e605c0a5076fd728ea2117c0e
Kevin Setayesh <kevinsetayesh@gmail.com>
2024-06-06 18:00:30 -0400

    Removed ToolTip component and css, now using built in MaterialUI ToolTip

--- packages/client/src/components/ReusableTable.tsx
+++ packages/client/src/components/ReusableTable.tsx
@@ -8,7 +8,6 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
 import { Link } from 'react-router-dom';
-import '../styles/Tooltip.css';
 import { InputType, ensureAbsoluteUrl, renderCellData } from '../constants/Constant';
 import ExportCSVButton from './ExportCSVButton';
 import { AbstractTable, TablesConfig } from '../tables/AbstractTable';
