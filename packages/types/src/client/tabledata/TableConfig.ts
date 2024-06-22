0db12867063ca8d202a7a026da2e64b816e9a439
Kevin Setayesh <kevinsetayesh@gmail.com>
2024-06-22 13:16:51 -0400

    Added a TableConfig.ts

--- packages/types/src/client/tabledata/TableConfig.ts
+++ packages/types/src/client/tabledata/TableConfig.ts
@@ -1,49 +1,105 @@
-import { TableType } from "./TableColumnConfig";
+import { ColumnDetail } from "../types/ClientTypes";
 
-export type TableDetailType = {
+
+export enum TableType {
+    PROPERTY_LIST_TABLE = 'PROPERTY_LIST_TABLE',
+    INVESTMENT_BREAKDOWN_TABLE = 'INVESTMENT_BREAKDOWN_TABLE',
+    AGENT_TABLE = 'AGENT_TABLE',
+    HIGH_YIELD_SAVINGS_TABLE = 'HIGH_YIELD_SAVINGS_TABLE',
+}
+
+export enum InvestmentBreakdownTableType {
+    STANDARD_BREAKDOWN = "STANDARD_BREAKDOWN",
+    MORTGAGE_BREAKDOWN = "MORTGAGE_BREAKDOWN",
+    EXPENSES_BREAKDOWN = "EXPENSES_BREAKDOWN",
+    INVESTMENT_BREAKDOWN = "INVESTMENT_BREAKDOWN",
+}
+
+export enum PropertiesListTableType {
+    ALL = 'ALL',
+    STANDARD_BREAKDOWN = "STANDARD_BREAKDOWN",
+}
+
+export enum DefaultTableType {
+    DEFAULT = 'DEFAULT',
+}
+
+export type TableTypeMapping = {
+    [TableType.PROPERTY_LIST_TABLE]: {
+        [key in PropertiesListTableType]: ColumnDetail[]
+    };
+    [TableType.INVESTMENT_BREAKDOWN_TABLE]: {
+        [key in InvestmentBreakdownTableType]: ColumnDetail[]
+    };
+    [TableType.AGENT_TABLE]: {
+        [key in DefaultTableType]: ColumnDetail[]
+    };
+    [TableType.HIGH_YIELD_SAVINGS_TABLE]: {
+        [key in DefaultTableType]: ColumnDetail[]
+    };
+};
+
+export type TableDetailType<T extends keyof TableTypeMapping> = {
     title: string;
-    tableType: TableType;
+    tableType: T;
     isEditable: boolean;
     canDeleteFromTable: boolean;
     isSortable: boolean;
     pageable: boolean;
+    subTables: TableTypeMapping[T];
 };
 
-export type TableDetailsType = {
-    [key in TableType]: TableDetailType;
+type TableDetailsType = {
+    [K in TableType]: TableDetailType<K>;
 };
 
 export const tableDetails: TableDetailsType = {
     [TableType.PROPERTY_LIST_TABLE]: {
-        title: '',
+        title: 'Property List',
         tableType: TableType.PROPERTY_LIST_TABLE,
         isEditable: true,
         canDeleteFromTable: true,
         isSortable: true,
         pageable: true,
+        subTables: {
+            [PropertiesListTableType.ALL]: [],
+            [PropertiesListTableType.STANDARD_BREAKDOWN]: [],
+        },
     },
     [TableType.INVESTMENT_BREAKDOWN_TABLE]: {
-        title: '',
+        title: 'Investment Breakdown',
         tableType: TableType.INVESTMENT_BREAKDOWN_TABLE,
         isEditable: false,
         canDeleteFromTable: false,
         isSortable: false,
         pageable: true,
+        subTables: {
+            [InvestmentBreakdownTableType.STANDARD_BREAKDOWN]: [],
+            [InvestmentBreakdownTableType.EXPENSES_BREAKDOWN]: [],
+            [InvestmentBreakdownTableType.MORTGAGE_BREAKDOWN]: [],
+            [InvestmentBreakdownTableType.INVESTMENT_BREAKDOWN]: [],
+        },
     },
     [TableType.AGENT_TABLE]: {
-        title: '',
+        title: 'Agent',
         tableType: TableType.AGENT_TABLE,
         isEditable: true,
         canDeleteFromTable: true,
         isSortable: true,
         pageable: true,
+        subTables: {
+            [DefaultTableType.DEFAULT]: [],
+        },
     },
     [TableType.HIGH_YIELD_SAVINGS_TABLE]: {
-        title: '',
+        title: 'High Yield Savings',
         tableType: TableType.HIGH_YIELD_SAVINGS_TABLE,
         isEditable: false,
         canDeleteFromTable: false,
         isSortable: false,
         pageable: true,
+        subTables: {
+            [DefaultTableType.DEFAULT]: [],
+        },
     },
 };
