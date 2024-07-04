adbf0b890da2965c039b2cb0b834e3dbf8957067
Kevin Setayesh <kevinsetayesh@gmail.com>
2024-07-03 22:38:56 -0400

    Working on adding new version of DummyCSVData table

---
+++ packages/client/src/newtabledata/tablecolumns/DummyCSVDataTableColumns.ts
@@ -0,0 +1,446 @@
+import { ColumnDetail, InputType } from "@realestatemanager/types";
+import { AddPropertyTitlesAndLabelsGetter } from "@realestatemanager/types";
+
+const getterInstance: AddPropertyTitlesAndLabelsGetter = new AddPropertyTitlesAndLabelsGetter();
+
+export const ZillowUrlColumn: ColumnDetail = {
+    title: getterInstance.zillowURLTitle,
+    accessor: getterInstance.zillowURLName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const FullAddressColumn: ColumnDetail = {
+    title: getterInstance.fullAddressTitle,
+    accessor: getterInstance.fullAddressName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const StateColumn: ColumnDetail = {
+    title: getterInstance.stateTitle,
+    accessor: getterInstance.stateName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ZipCodeColumn: ColumnDetail = {
+    title: getterInstance.zipcodeTitle,
+    accessor: getterInstance.zipcodeName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const CityColumn: ColumnDetail = {
+    title: getterInstance.cityTitle,
+    accessor: getterInstance.cityName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const CountryColumn: ColumnDetail = {
+    title: getterInstance.countryTitle,
+    accessor: getterInstance.countryName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const StreetAddressColumn: ColumnDetail = {
+    title: getterInstance.streetAddressTitle,
+    accessor: getterInstance.streetAddressName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ApartmentNumberColumn: ColumnDetail = {
+    title: getterInstance.apartmentNumberTitle,
+    accessor: getterInstance.apartmentNumberName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const LongitudeColumn: ColumnDetail = {
+    title: getterInstance.longitudeTitle,
+    accessor: getterInstance.longitudeName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const LatitudeColumn: ColumnDetail = {
+    title: getterInstance.latitudeTitle,
+    accessor: getterInstance.latitudeName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const NumberOfDaysOnMarketColumn: ColumnDetail = {
+    title: getterInstance.numberOfDaysOnMarketTitle,
+    accessor: getterInstance.numberOfDaysOnMarketName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ElementarySchoolRatingColumn: ColumnDetail = {
+    title: getterInstance.elementarySchoolRatingTitle,
+    accessor: getterInstance.elementarySchoolRatingName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const MiddleSchoolRatingColumn: ColumnDetail = {
+    title: getterInstance.middleSchoolRatingTitle,
+    accessor: getterInstance.middleSchoolRatingName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const HighSchoolRatingColumn: ColumnDetail = {
+    title: getterInstance.highSchoolRatingTitle,
+    accessor: getterInstance.highSchoolRatingName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const NumberOfBedroomsColumn: ColumnDetail = {
+    title: getterInstance.numberOfBedroomsTitle,
+    accessor: getterInstance.numberOfBedroomsName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const NumberOfFullBathroomsColumn: ColumnDetail = {
+    title: getterInstance.numberOfFullBathroomsTitle,
+    accessor: getterInstance.numberOfFullBathroomsName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const NumberOfHalfBathroomsColumn: ColumnDetail = {
+    title: getterInstance.numberOfHalfBathroomsTitle,
+    accessor: getterInstance.numberOfHalfBathroomsName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const SquareFeetColumn: ColumnDetail = {
+    title: getterInstance.squareFeetTitle,
+    accessor: getterInstance.squareFeetName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const AcresColumn: ColumnDetail = {
+    title: getterInstance.acresTitle,
+    accessor: getterInstance.acresName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const YearBuiltColumn: ColumnDetail = {
+    title: getterInstance.yearBuiltTitle,
+    accessor: getterInstance.yearBuiltName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const HasGarageColumn: ColumnDetail = {
+    title: getterInstance.hasGarageTitle,
+    accessor: getterInstance.hasGarageName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const HasPoolColumn: ColumnDetail = {
+    title: getterInstance.hasPoolTitle,
+    accessor: getterInstance.hasPoolName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const HasBasementColumn: ColumnDetail = {
+    title: getterInstance.hasBasementTitle,
+    accessor: getterInstance.hasBasementName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const PropertyTypeColumn: ColumnDetail = {
+    title: getterInstance.propertyTypeTitle,
+    accessor: getterInstance.propertyTypeName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const PropertyStatusColumn: ColumnDetail = {
+    title: getterInstance.propertyStatusTitle,
+    accessor: getterInstance.propertyStatusName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ListingPriceColumn: ColumnDetail = {
+    title: getterInstance.listingPriceTitle,
+    accessor: getterInstance.listingPriceName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ZestimateColumn: ColumnDetail = {
+    title: getterInstance.zestimateTitle,
+    accessor: getterInstance.zestimateName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ZillowRentEstimateColumn: ColumnDetail = {
+    title: getterInstance.zillowRentEstimateTitle,
+    accessor: getterInstance.zillowRentEstimateName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ZestimateRangeLowColumn: ColumnDetail = {
+    title: getterInstance.zestimateRangeLowTitle,
+    accessor: getterInstance.zestimateRangeLowName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ZestimateRangeHighColumn: ColumnDetail = {
+    title: getterInstance.zestimateRangeHighTitle,
+    accessor: getterInstance.zestimateRangeHighName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ZillowMonthlyPropertyTaxAmountColumn: ColumnDetail = {
+    title: getterInstance.zillowMonthlyPropertyTaxAmountTitle,
+    accessor: getterInstance.zillowMonthlyPropertyTaxAmountName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ZillowMonthlyHomeInsuranceAmountColumn: ColumnDetail = {
+    title: getterInstance.zillowMonthlyHomeInsuranceAmountTitle,
+    accessor: getterInstance.zillowMonthlyHomeInsuranceAmountName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const ZillowMonthlyHOAFeesAmountColumn: ColumnDetail = {
+    title: getterInstance.zillowMonthlyHOAFeesAmountTitle,
+    accessor: getterInstance.zillowMonthlyHOAFeesAmountName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
+
+export const DescriptionColumn: ColumnDetail = {
+    title: getterInstance.descriptionTitle,
+    accessor: getterInstance.descriptionName,
+    inputType: InputType.STRING,
+    isUrl: false,
+    isDollarAmount: false,
+    addSuffix: "",
+    showColumn: true,
+    isEditable: false,
+    isSortable: false,
+    detailedDescription: "",
+};
---
+++ packages/client/src/newtabledata/tabledata/DummyCSVDataTable.ts
@@ -0,0 +1,59 @@
+import { AgentResponseDTO, ColumnDetail, CreateUpdateAgentRequest, DefaultTableType, PrimitiveType, TableColumn, TableColumnDetailsEnum, TableType } from "@realestatemanager/types";
+import { AbstractTable } from "./AbstractTable";
+
+export class DummyCSVDataTable extends AbstractTable<TableType.AGENT_TABLE, AgentResponseDTO, DefaultTableType> {
+
+    constructor() {
+        super(TableType.AGENT_TABLE);
+    }
+
+    getDefaultTableType(): DefaultTableType {
+        return DefaultTableType.DEFAULT;
+    }
+
+    protected _getAllSubTableColumns(subTableType: DefaultTableType): TableColumnDetailsEnum[] {
+        return this.subTables[subTableType];
+    }
+
+    protected getColumnValue(
+        item: AgentResponseDTO,
+        tableColumn: TableColumn,
+    ): PrimitiveType {
+        const columnDetail: ColumnDetail = tableColumn.columnDetails;
+        if (columnDetail[TableType.AGENT_TABLE]) {
+            const { value } = columnDetail[TableType.AGENT_TABLE]!;
+            return value(item);
+        }
+        throw new Error(`Column ${tableColumn.columnKey} does not have a value function for ${TableType.AGENT_TABLE}`);
+    }
+
+    protected setColumnValue(
+        item: AgentResponseDTO,
+        newValue: PrimitiveType,
+        tableColumn: TableColumn,
+    ): void {
+        const columnDetail: ColumnDetail = tableColumn.columnDetails;
+        if (columnDetail[TableType.AGENT_TABLE]) {
+            const { setValue } = columnDetail[TableType.AGENT_TABLE];
+            if (setValue) {
+                setValue(item, newValue);
+                return;
+            }
+        }
+        throw new Error(`Column ${tableColumn.columnKey} does not have a setValue function for ${TableType.AGENT_TABLE}`);
+    }
+
+    createUpdateAgentRequest(agent: AgentResponseDTO): CreateUpdateAgentRequest {
+        return {
+            id: agent.id,
+            firstName: agent.firstName,
+            lastName: agent.lastName,
+            website: agent.website,
+            companyName: agent.companyName,
+            phoneNumber: agent.phoneNumber,
+            email: agent.email,
+        };
+    }
+
+
+}
